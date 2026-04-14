cdf52372772959bb247186b940a19ebcfba5b882
minnulekha <minnulekhavg@gmail.com>
2026-04-14 16:03:02 +0530

    Final Touch

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const slideImg = document.getElementById('slideshow-img');
     const skipBtn = document.getElementById('skip-slideshow-btn');
     let slideIndex = 1; 
-    const totalPhotos = 27;
+    const totalPhotos = 23;
     let slideshowInterval; 
 
     function goToNextSlide() {
